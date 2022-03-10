41db0716975539209d1c8fc25357721f11311383
Julian Lee <2600433l@student.gla.ac.uk>
2022-03-10 21:18:39 +0000

    home page

--- workspace/concept_io_project/concept_io_project/urls.py
+++ workspace/concept_io_project/concept_io_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from concept_io_project import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
+
