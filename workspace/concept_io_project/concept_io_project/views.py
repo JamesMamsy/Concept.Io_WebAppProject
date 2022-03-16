2739aad7b2136893223d237b68a8e41857d0e58b
Anthony Rainey <2506467r@student.gla.ac.uk>
2022-03-16 17:27:38 +0000

    Login

--- workspace/concept_io_project/concept_io_project/views.py
+++ workspace/concept_io_project/concept_io_project/views.py
@@ -11,3 +11,7 @@ def index(request):
     context_dict['pages'] = [page_list]
     #context_dict['additions'] = [new pages]
     return render(request, 'rango/index.html', context = context_dict)
+    
+def login(request):
+
+    return render(request, 'rango/login.html')    
