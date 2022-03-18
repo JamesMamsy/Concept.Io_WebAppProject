144f61cad0ef2689d5ae475b90ce550dfa2b5552
Ioannis Kyriacou <2462278k@student.gla.ac.uk>
2022-03-18 15:02:44 +0000

    Models and views

--- workspace/concept_io_project/rango/models.py
+++ workspace/concept_io_project/rango/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField('Name', max_length=120)
+    noProjects=0
+
--- workspace/concept_io_project/rango/views.py
+++ workspace/concept_io_project/rango/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def view_categories(request):
+    context_dict = {}
+
+    categories=Category.objects.all()
+    conttext_dict['categories']=categories
+
+    return render(request, 'rango/categories.html',context_dict)
+
+
+
